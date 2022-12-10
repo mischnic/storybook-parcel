cba30aa6e1845dae821d139ddee7ad1771ef5584
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2022-12-10 13:38:53 +0100

    Initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+const path = require('path');
+module.exports = {
+  "stories": [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  },
+  "docs": {
+    "docsPage": true
+  }
+}
